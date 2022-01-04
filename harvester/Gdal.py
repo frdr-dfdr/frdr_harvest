76ec553028229f81e87ff7db9e14b9e12cc6a400
Paul Dante <paul.dante@ubc.ca>
2022-01-04 01:27:14 -0800

    Added commands to add GDAL to VM environment

--- harvester/Gdal.py
+++ harvester/Gdal.py
@@ -1,5 +1,13 @@
 from osgeo import gdal
 
+# Need to install GDAL on VM for this to work
+    # sudo add-apt-repository ppa:ubuntugis/ppa && sudo apt-get update
+    # sudo apt-get update
+    # sudo apt-get install gdal-bin
+    # sudo apt-get install libgdal-dev
+    # export CPLUS_INCLUDE_PATH=/usr/include/gdal
+    # export C_INCLUDE_PATH=/usr/include/gdal
+    # pip install GDAL
 
 def utm_2_lat_long(file_path):
     dataset = gdal.Open(file_path)
