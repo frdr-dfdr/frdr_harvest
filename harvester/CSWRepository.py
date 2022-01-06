a711c998c149e59743c3b240da32a9995683dbfd
Kelly Stathis <kelly.a.stathis@gmail.com>
2022-01-05 19:23:48 -0800

    CSWRepository: Switch to using ISO 19139 metadata retrieved from geonetwork API (not owslib)

--- harvester/CSWRepository.py
+++ harvester/CSWRepository.py
@@ -1,14 +1,11 @@
 from harvester.HarvestRepository import HarvestRepository
 from harvester.rate_limited import rate_limited
-from owslib.csw import CatalogueServiceWeb
 import time
-import re
-import warnings
-
-# Disable FutureWarning from owslib - we do not use the properties listed in
-# https://github.com/geopython/OWSLib/issues/38 and
-# https://github.com/geopython/OWSLib/issues/301
-warnings.simplefilter("ignore", category=FutureWarning)
+import requests
+import lxml.etree as ET
+from rdflib import Graph, DCAT
+from dateutil import parser
+from uuid import UUID
 
 class CSWRepository(HarvestRepository):
     """ CSW Repository """
@@ -16,11 +13,8 @@ class CSWRepository(HarvestRepository):
     def setRepoParams(self, repoParams):
         self.metadataprefix = "csw"
         super(CSWRepository, self).setRepoParams(repoParams)
-        try:
-            self.cswrepo = CatalogueServiceWeb(self.url)
-        except Exception as e:
-            self.cswrepo = None
         self.domain_metadata = []
+        self.headers = {'accept': 'application/rdf+xml'}
 
     def _crawl(self):
         kwargs = {
@@ -37,100 +31,214 @@ class CSWRepository(HarvestRepository):
         }
         self.repository_id = self.db.update_repo(**kwargs)
 
-        if self.cswrepo is None:
-            self.logger.error("Could not initiate this repo to crawl it")
-            return
-
-        item_count = 0
-        while True:
-            try:
-                self.cswrepo.getrecords2(startposition=self.cswrepo.results['nextrecord'])
-            except Exception as e:
-                self.cswrepo.getrecords2()
-
-            for rec in self.cswrepo.records:
-                self.db.write_header(self.cswrepo.records[rec].identifier, self.item_url_pattern, self.repository_id)
+        try:
+            g = Graph()
+            g.parse(self.url) # FIXME handle response 400 here too
+            item_count = 0
+            for s, p, o in g.triples((None, DCAT.dataset, None)):
+                split_url = "https://hecate.hakai.org/geonetwork/srv/metadata//datasets/"
+                if "https://hecate.hakai.org/geonetwork/srv/metadata/" in o.split(split_url)[-1]:
+                    identifier = o.split(split_url)[-1].split("https://hecate.hakai.org/geonetwork/srv/metadata/")[-1]
+                else:
+                    identifier = o.split(split_url)[-1]
+                try:
+                    uuid = UUID(identifier)
+                except ValueError:
+                    self.logger.error("Not a UUID: {}".format(identifier))
+                    # FIXME hande this case - find the uuid elsewhere
+                    continue
+                self.db.write_header(identifier, self.item_url_pattern, self.repository_id)
                 item_count = item_count + 1
                 if (item_count % self.update_log_after_numitems == 0):
                     tdelta = time.time() - self.tstart + 0.1
                     self.logger.info("Done {} item headers after {} ({:.1f} items/sec)".format(
                         item_count, self.formatter.humanize(tdelta), item_count / tdelta))
-            if item_count >= self.cswrepo.results['matches']:
-                break
+            self.logger.info("Found {} items in feed".format(item_count))
+            return True
 
-        self.logger.info("Found {} items in feed".format(item_count))
+        except Exception as e:
+            self.logger.error("Updating CSW Repository failed: {} {}".format(type(e).__name__, e))
+            self.error_count = self.error_count + 1
+            if self.error_count < self.abort_after_numerrors:
+                return True
+        return False
 
     def format_csw_to_oai(self, csw_record, local_identifier):
+        def get_gco_CharacterString(element):
+            if isinstance(element, ET._Element):
+                if element.find("gco:CharacterString", csw_record.nsmap) is not None:
+                    return element.find("gco:CharacterString", csw_record.nsmap).text
+            return None
+
         record = {}
 
-        if csw_record.language == "eng":
-            record["title"] = csw_record.title
-            record["title"] = record["title"].strip()
-            record["title_fr"] = ""
-            record["tags"] = csw_record.subjects
-        elif csw_record.language == "fre":
-            record["title_fr"] = csw_record.title
-            record["title_fr"] = record["title_fr"].strip()
-            record["title"] = ""
-            record["tags_fr"] = csw_record.subjects
+        # todo Examples for reference:
+        "https://hecate.hakai.org/geonetwork/srv/api/records/23bc8c35-2e4e-4382-9296-a52d5ea49889/formatters/xml"
+        "https://hecate.hakai.org/geonetwork/srv/api/records/e2d3d616-9ee2-451f-8584-14801b4c6fd0/formatters/xml"
 
-        record["description"] = csw_record.abstract
+        # todo https://docs.meridian.cs.dal.ca/metadata/Metadata.html is useful as a rough guide to ISO 19115, although they made changes
+
+
+        # Item URL: use DOI if available
+        record["item_url"] = self.item_url_pattern.replace("%id%", local_identifier)
+        if csw_record.find("gmd:dataSetURI", csw_record.nsmap) is not None:
+            record["item_url"] = csw_record.find("gmd:dataSetURI", csw_record.nsmap).find("gco:CharacterString",csw_record.nsmap).text
+
+        # Shortcuts to frequently used nodes:
+        data_identification = csw_record.find("gmd:identificationInfo", csw_record.nsmap).find("gmd:MD_DataIdentification", csw_record.nsmap)
+        citation = data_identification.find("gmd:citation", csw_record.nsmap).find("gmd:CI_Citation", csw_record.nsmap)
+
+        # Creators and affiliations
+        ci_responsible_parties = []
+        record["creators"] = []
+        record["affiliations"] = []
+
+        # gmd:contact / gmd:CI_ResponsibleParty
+        ci_responsible_parties.extend(csw_record.find("gmd:contact", csw_record.nsmap).findall("gmd:CI_ResponsibleParty", csw_record.nsmap))
+
+        # gmd:identificationInfo / gmd:MD_DataIdentification / gmd:citation / gmd:CI_Citation / gmd:citedResponsibleParty / gmd:CI_ResponsibleParty
+        for citedResponsibleParty in citation.findall("gmd:citedResponsibleParty", csw_record.nsmap):
+            ci_responsible_parties.append(citedResponsibleParty.find("gmd:CI_ResponsibleParty", csw_record.nsmap))
+
+        # gmd:identificationInfo / gmd:MD_DataIdentification / gmd:pointOfContact / gmd:CI_ResponsibleParty
+        for pointOfContact in data_identification.findall("gmd:pointOfContact", csw_record.nsmap):
+            ci_responsible_parties.append(pointOfContact.find("gmd:CI_ResponsibleParty", csw_record.nsmap))
+
+        for ci_responsible_party in ci_responsible_parties:
+            if ci_responsible_party.find("gmd:individualName", csw_record.nsmap) is not None:
+                record["creators"].append(get_gco_CharacterString(ci_responsible_party.find("gmd:individualName", csw_record.nsmap)))
+                if ci_responsible_party.find("gmd:organisationName", csw_record.nsmap) is not None:
+                    record["affiliations"].append(get_gco_CharacterString(ci_responsible_party.find("gmd:organisationName", csw_record.nsmap)))
+
+        # Remove duplicates
+        record["creators"] = list(set(record["creators"]))
+        record["affiliations"] = list(set(record["affiliations"]))
+
+        # Title
+        record["title"] = get_gco_CharacterString(citation.find("gmd:title", csw_record.nsmap)).strip()
+        record["title_fr"] = ""
+
+        # Description
+        record["description"] = get_gco_CharacterString(data_identification.find("gmd:abstract", csw_record.nsmap))
+
+        # Publication date
+        citation_dates = {}
+        for date in citation.findall("gmd:date", csw_record.nsmap):
+            ci_date = date.find("gmd:CI_Date", csw_record.nsmap)
+            try:
+                date_value = ""
+                if ci_date.find("gmd:date", csw_record.nsmap).find("gco:Date", csw_record.nsmap) is not None:
+                    date_value = parser.parse(ci_date.find("gmd:date", csw_record.nsmap).find("gco:Date", csw_record.nsmap).text).strftime('%Y-%m-%d')
+                elif ci_date.find("gmd:date", csw_record.nsmap).find("gco:DateTime", csw_record.nsmap) is not None:
+                    date_value = parser.parse(ci_date.find("gmd:date", csw_record.nsmap).find("gco:DateTime", csw_record.nsmap).text).strftime('%Y-%m-%d')
+                date_type = ci_date.find("gmd:dateType", csw_record.nsmap).find("gmd:CI_DateTypeCode", csw_record.nsmap).attrib["codeListValue"]
+                citation_dates[date_type] = date_value
+            except AttributeError as e:
+                self.logger.error("Record {} encountered error parsing CI_Date: {}".format(local_identifier, e))
+
+        if "publication" in citation_dates.keys():
+            record["pub_date"] = citation_dates["publication"]
+        elif "revision" in citation_dates.keys():
+            record["pub_date"] = citation_dates["revision"]
+        elif "creation" in citation_dates.keys():
+            record["pub_date"] = citation_dates["creation"]
+        else:
+            self.logger.error("Record {} missing publication, revision, and creation dates")  # TODO investigate if this happens
+
+
+
+        # TODO  keywords - tags, subjects(?)
+        record["tags"] = []
+        record["subject"] = []
+        # TODO rights
+        # TODO access
+
+        language = ""
+        if csw_record.find("gmd:language", csw_record.nsmap).find("gmd:LanguageCode", csw_record.nsmap) is not None:
+            language = csw_record.find("gmd:language", csw_record.nsmap).find("gmd:LanguageCode", csw_record.nsmap).attrib["codeListValue"]
+        elif get_gco_CharacterString(csw_record.find("gmd:language", csw_record.nsmap)) is not None:
+            language = get_gco_CharacterString(csw_record.find("gmd:language", csw_record.nsmap))
+
+        if language == "fre":
+            record["title_fr"] = record["title"]
+            record["title"] = ""
+            record["tags_fr"] = record["tags"]
+            record["tags"] = []
+            record["subject_fr"] = record["subject"]
+            record["subject"] = []
+            record["description_fr"] = record["description"]
+            record["description"] = ""
+
         record["identifier"] = local_identifier
-        record["creator"] = self.name
         record["series"] = ""
 
-        if csw_record.bbox:
-            # Workaround to address issue in oswlib related to EPSG:4326 CRS code that flips coordinates
-            if float(csw_record.bbox.minx) > float(csw_record.bbox.maxx):
-                # longitude values (minx and maxx) are switched by oswlib; switch them back
-                record["geobboxes"] = [{"southLat": csw_record.bbox.miny, "westLon": csw_record.bbox.maxx,
-                                        "northLat": csw_record.bbox.maxy, "eastLon": csw_record.bbox.minx}]
-            elif float(csw_record.bbox.miny) > float(csw_record.bbox.maxy):
-                # sometimes x and y values are switched, so the lats are longs and vice versa
-                # we can look for the same discrepancy that happens in the longs, except it's in the y values now
-                record["geobboxes"] = [{"southLat": csw_record.bbox.minx, "westLon": csw_record.bbox.maxy,
-                                       "northLat": csw_record.bbox.maxx, "eastLon": csw_record.bbox.miny}]
-            else:
-                # default if nothing is wrong (this code isn't executed currently)
-                record["geobboxes"] = [{"southLat": csw_record.bbox.miny, "westLon": csw_record.bbox.minx,
-                                        "northLat": csw_record.bbox.maxy, "eastLon": csw_record.bbox.maxx}]
+        # TODO update to use iso 19139
+        # if csw_record.bbox:
+        #     # Workaround to address issue in oswlib related to EPSG:4326 CRS code that flips coordinates
+        #     if float(csw_record.bbox.minx) > float(csw_record.bbox.maxx):
+        #         # longitude values (minx and maxx) are switched by oswlib; switch them back
+        #         record["geobboxes"] = [{"southLat": csw_record.bbox.miny, "westLon": csw_record.bbox.maxx,
+        #                                 "northLat": csw_record.bbox.maxy, "eastLon": csw_record.bbox.minx}]
+        #     elif float(csw_record.bbox.miny) > float(csw_record.bbox.maxy):
+        #         # sometimes x and y values are switched, so the lats are longs and vice versa
+        #         # we can look for the same discrepancy that happens in the longs, except it's in the y values now
+        #         record["geobboxes"] = [{"southLat": csw_record.bbox.minx, "westLon": csw_record.bbox.maxy,
+        #                                "northLat": csw_record.bbox.maxx, "eastLon": csw_record.bbox.miny}]
+        #     else:
+        #         # default if nothing is wrong (this code isn't executed currently)
+        #         record["geobboxes"] = [{"southLat": csw_record.bbox.miny, "westLon": csw_record.bbox.minx,
+        #                                 "northLat": csw_record.bbox.maxy, "eastLon": csw_record.bbox.maxx}]
 
         return record
 
     @rate_limited(5)
     def _update_record(self, record):
-        if self.cswrepo is None:
-            return
+        request_success = False
+        request_count = 0
+        while not request_success and request_count < 5:
+            try:
+                xml_record_url = "https://hecate.hakai.org/geonetwork/srv/api/records/{}/formatters/xml".format(record["local_identifier"])
+                response = requests.request("GET", xml_record_url)
+                if response.status_code == 200:
+                    request_success = True
+                elif response.status_code == 400:
+                    request_count += 1
+                    time.sleep(1) # FIXME not sure if this is the best way to wait
+                else:
+                    break
+            except Exception as e:
+                self.logger.error("Unable to update record {}: {} {}".format(record['local_identifier'], type(e).__name__, e))
+                self.db.delete_record(record)
+                return False
 
-        try:
-            self.cswrepo.getrecordbyid(id=[record['local_identifier']])
-        except Exception as e:
-            self.logger.error("Unable to update record {}: {} {}".format(record['local_identifier'], type(e).__name__, e))
-            self.db.delete_record(record)
-            return False
+        if response.status_code == 200:
+            try:
+                xml_record = ET.fromstring(str.encode(response.text))
+            except ET.XMLSyntaxError as e:
+                self.logger.error("Unable to parse record {}: {}, {}".format(record['local_identifier'], type(e).__name__, e))
+                self.db.touch_record(record) # Record has XML error, try again later
+                return True
+            oai_record = self.format_csw_to_oai(xml_record, record["local_identifier"])
 
-        if self.cswrepo.records:
-            csw_record = self.cswrepo.records[record['local_identifier']]
-            oai_record = self.format_csw_to_oai(csw_record, record['local_identifier'])
-            # We have to request a second schema to get valid dates, no idea if issue is Hakai-specific
-            self.cswrepo.getrecordbyid(id=[record['local_identifier']], outputschema="http://www.isotc211.org/2005/gmd")
-            oai_record["pub_date"] = self.cswrepo.records[record['local_identifier']].datestamp
-            oai_record["pub_date"] = re.sub("[T ][0-9][0-9]:[0-9][0-9]:[0-9][0-9]\.?[0-9]*[Z]?$", "",
-                                            oai_record["pub_date"])
             if oai_record:
                 try:
                     self.db.write_record(oai_record, self)
                 except Exception as e:
-                    self.logger.error("Updating record {} failed: {} {}".format(record['local_identifier'], type(e).__name__, e))
+                    self.logger.error(
+                        "Updating record {} failed: {} {}".format(record['local_identifier'], type(e).__name__, e))
                     if self.dump_on_failure == True:
                         try:
-                            print(csw_record)
+                            print(response.text)
                         except Exception as e:
                             pass
-
-        else:
-            # This record was deleted
+        elif response.status_code == 404:
+            # Record not found delete it
             self.db.delete_record(record)
+            return True
+        else: # includes 400, try again later
+            self.logger.error("Unable to fetch record {} after {} tries".format(record['local_identifier'], request_count))
+            self.db.touch_record(record)
+            return True
         
         return True
 
