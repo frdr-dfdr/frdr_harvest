3d7999c055abd1ee769b71c71128499f794dd4ed
Paul Dante <46582128+pdante-ubc@users.noreply.github.com>
2022-01-13 16:04:39 -0800

    Updated SQLite and added postgres updates

--- sql/postgres/20220111.sql
+++ sql/postgres/20220111.sql
@@ -1,21 +1,3 @@
-CREATE TABLE IF NOT EXISTS geonames (
-geonames_id INTEGER PRIMARY KEY NOT NULL,
-country text,
-province_state text,
-city text,
-northLat NUMERIC,
-southLat NUMERIC,
-eastLon NUMERIC,
-westLon NUMERIC,
-last_modified_timestamp int DEFAULT 0);
-
-ALTER TABLE geoplace
-ADD COLUMN record_uuid TEXT
-CONSTRAINT fk_uuid REFERENCES records(record_uuid)
-ON UPDATE CASCADE ON DELETE CASCADE;
-
-ALTER TABLE geoplace
-ADD COLUMN geonames_id INTEGER
 CONSTRAINT fk_geonames REFERENCES geonames(geonames_id)
 ON UPDATE CASCADE ON DELETE CASCADE;
 
@@ -31,5 +13,3 @@ ADD COLUMN geodisy_review_status INTEGER DEFAULT 0;
 UPDATE geoplace set record_uuid = (select record_uuid from records_x_geoplace where records_x_geoplace.geoplace_id = geoplace.geoplace_id);
 
 DROP TABLE records_x_geoplace;
-
-
--- sql/sqlite/20220102.sql
+++ sql/sqlite/20220102.sql
@@ -1,16 +1,4 @@
-CREATE TABLE geospatial_boundingbox(
-	geospatial_boundingbox_id INTEGER PRIMARY KEY NOT NULL,
-	record_id INTEGER NOT NULL,
-	geofile_id INTEGER,
-	westLon NUMERIC NOT NULL,
-	eastLon NUMERIC NOT NULL,
-    northLat NUMERIC NOT NULL,
-    southLat NUMERIC NOT NULL);
-
-INSERT INTO temp SELECT * FROM geospatial_boundingbox;
-DROP INDEX geospatial_boundingbox_by_record;
-DROP TABLE geospatial_boundingbox;
-ALTER TABLE temp TO geospatial_boundingbox;
-CREATE INDEX geospatial_boundingbox_by_record on geospatial_boundingbox(record_id);
-
-vacuum;
+ALTER TABLE geobbox
+ADD COLUMN geofile_id INTEGER
+CONSTRAINT fk_geofile REFERENCES geofile(geofile_id)
+ON UPDATE CASCADE ON DELETE CASCADE;
