31b6799368db4c34c22963fe3ad65ee47428749f
Paul Dante <46582128+pdante-ubc@users.noreply.github.com>
2022-01-13 16:24:50 -0800

    Fixing sql error

--- sql/sqlite/20220111.sql
+++ sql/sqlite/20220111.sql
@@ -28,7 +28,7 @@ ADD COLUMN geonames_associated_timestamp INTEGER DEFAULT 0;
 ALTER TABLE geoplace
 ADD COLUMN geodisy_review_status INTEGER DEFAULT 0;
 
-UPDATE geoplace set record_uuid = (select record_uuid from records_x_geoplace where records_x_geoplace.geoplace_id = geoplace.geoplace_id);
+UPDATE geoplace set record_uuid = (select record_uuid from records_x_geoplace where records_x_geoplace.geoplace_id = geoplace.geoplace_id limit 1);
 
 DROP TABLE records_x_geoplace;
 
