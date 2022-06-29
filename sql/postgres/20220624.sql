db5663f0165c517d76b7967905c2157a806a7216
Todd Trann <toddtrann@sasktel.net>
2022-06-29 11:24:42 -0600

    Update sql for Borealis

--- sql/postgres/20220624.sql
+++ sql/postgres/20220624.sql
@@ -10,9 +10,6 @@ update records set modified_timestamp = 0 where repository_id in (
     select repository_id from repositories where repository_url like '%borealisdata.ca%'
 );
 
-update repositories set repository_name = 'Other Borealis Dataverses'
+update repositories set repository_name = 'Other Borealis Collections', repository_thumbnail = REPLACE(repository_thumbnail,'sp_80x80','borealis_80x80')
 where repository_name = 'Other Scholars Portal Dataverses';
 
-UPDATE repositories
-SET repository_thumbnail = REPLACE(repository_thumbnail,'sp_80x80','borealis_80x80')
-WHERE repository_name = 'Other Borealis Dataverses';
